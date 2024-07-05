225c6ce77b7dd3b9f42428d10e23f3625de79c78
Felipe Mora <gnar2342@gmail.com>
2024-07-04 23:18:44 -0400

    [START] INITI BACKEND PORJECT

---
+++ api/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+
+app.listen(3001, () => {
+    console.info('toi parao en el puerto 3001');
+});
---
+++ api/migrations/20240705030017-create-user-2.js
@@ -0,0 +1,31 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('user2s', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      first_name: {
+        type: Sequelize.STRING
+      },
+      example2: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('user2s');
+  }
+};
