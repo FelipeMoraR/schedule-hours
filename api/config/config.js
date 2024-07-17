bd8b0bfdb27497871466b408facf1a4f2f1644bb
Felipe Mora <gnar2342@gmail.com>
2024-07-17 16:11:58 -0400

    [FIX] variables db config

--- api/config/config.js
+++ api/config/config.js
@@ -26,7 +26,7 @@ module.exports = {
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
     "host": process.env.DB_HOST,
-    "port": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
     "dialect": process.env.DB_DIALECT,
   }
 }
