757a5bc45233e6fb172b2afa64c94e46e5cc15ad
Felipe Mora <gnar2342@gmail.com>
2024-07-17 15:52:02 -0400

    [ADD] parameters to db conection.

--- api/config/config.js
+++ api/config/config.js
@@ -11,6 +11,7 @@ module.exports = {
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
     "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
     "dialect": process.env.DB_DIALECT,
   },
   "test": {
@@ -21,11 +22,12 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_HOST,
+    "dialect": process.env.DB_DIALECT,
   }
 }
 
