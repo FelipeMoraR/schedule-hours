cbb9bae9f4895aea3324ad13fd2a260800019af7
Felipe Mora <gnar2342@gmail.com>
2024-07-17 23:06:07 -0400

    [ADD] changes to run back and front at the same time.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // Escucha en todas las interfaces
+    host: true, // Escucha en todas las interfaces
     port: 5173,
   },
 })
