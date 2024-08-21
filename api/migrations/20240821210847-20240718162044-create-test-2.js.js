ffee771bfb45fa01509401a33f80df9918bde668
Felipe Mora <80855125+FelipeMoraR@users.noreply.github.com>
2024-08-21 17:11:32 -0400

    [FIX] RESTORING DB

---
+++ api/migrations/20240821210847-20240718162044-create-test-2.js.js
@@ -0,0 +1,31 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('test2s', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nombre1: {
+        type: Sequelize.STRING
+      },
+      nombre2: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('test2s');
+  }
+};
